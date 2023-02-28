ea8307e1c66afb2f17b4db42d3144f95a2d12e01
ChaoqunSu <1728448550@qq.com>
2023-02-28 18:27:07 +0800

    704_2

--- 704.二分查找.py
+++ 704.二分查找.py
@@ -7,16 +7,17 @@
 # @lc code=start
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums) - 1
+        # 定义在左闭右闭的闭区间里面，这时不能用left=mid而是=mid+1
+        left, right = 0, len(nums)-1
         while left <= right:
-            mid = (right - left) // 2 + left
-            num = nums[mid]
-            if num == target:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
                 return mid
-            elif num > target:
-                right = mid - 1
-            else:
+            elif nums[mid] < target:
                 left = mid + 1
+            else:
+                right = mid - 1
+        
         return -1
 # @lc code=end
         

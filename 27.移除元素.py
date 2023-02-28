aad7c60171bea6ed95a7953c9444c00d5a7b2545
ChaoqunSu <1728448550@qq.com>
2023-02-28 21:17:54 +0800

    27_2

--- 27.移除元素.py
+++ 27.移除元素.py
@@ -8,10 +8,10 @@
 class Solution:
     # 2
     def removeElement(self, nums: List[int], val: int) -> int:
-        slow = 0
-        for fast in range (len(nums)):
-            if nums[fast] != val:
-                nums[slow] = nums[fast]
+        slow = 0 
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[slow] = nums[i]
                 slow += 1
         return slow
 # @lc code=end

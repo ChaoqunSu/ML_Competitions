51fe09a6ed536437249c423c39fde0b77c7f76c1
ChaoqunSu <1728448550@qq.com>
2023-03-08 15:47:37 +0800

    145

---
+++ 145.二叉树的后序遍历.py
@@ -0,0 +1,33 @@
+#
+# @lc app=leetcode.cn id=145 lang=python3
+#
+# [145] 二叉树的后序遍历
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        """
+        后序遍历是:左右中
+        """
+        # 递归函数
+        def postorder(root:TreeNode):
+            if root == None:
+                return
+            postorder(root.left)
+            postorder(root.right)
+            result.append(root.val)
+
+        # 结果列表
+        result = []
+        postorder(root)
+        return result
+
+# @lc code=end
+
